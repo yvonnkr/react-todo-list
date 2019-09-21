5e4cf53f1ce400b17e43453a699c6969c7b4992b
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-21 11:36:23 +0100

    Init commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello from TodoItem</p>
+      </div>
+    );
+  }
+}
