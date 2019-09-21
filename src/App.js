f1a1ed45387821d6b11263972274d15b2f71d4be
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-21 12:37:48 +0100

    App Component Structure

--- src/App.js
+++ src/App.js
@@ -7,14 +7,51 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Visual Studio Code Github Setup
 class App extends Component {
-  state = {};
+  state = {
+    items: [{ id: 1, title: 'wake up' }, { id: 2, title: 'shopping' }],
+    id: uuid(),
+    item: '',
+    editItem: false
+  };
+
+  handleChange = e => {
+    console.log('handleChange');
+  };
+
+  handleSubmit = e => {
+    console.log('handleSubmit');
+  };
+
+  handleDelete = id => {
+    console.log(`handleDelete ${id}`);
+  };
+
+  handleEdit = id => {
+    console.log(`handleEdit ${id}`);
+  };
+
+  clearList = () => {
+    console.log('clearList');
+  };
+
   render() {
     return (
-      <div>
-        <div className='container'>
-          <div className='row'>
-            <TodoInput />
-            <TodoList />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10 mx-auto col-md-8 mt-5'>
+            <h3 className='text-capitalize text-center'>Todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
           </div>
         </div>
       </div>
